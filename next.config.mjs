0357726e7a9970690aea467899b85f3781da41cc
Gigioxx <guillermo.casanova.b@gmail.com>
2025-05-26 23:38:30 -0400

    feat: add additional image domain for coin images to enhance image loading capabilities

--- next.config.mjs
+++ next.config.mjs
@@ -54,7 +54,7 @@ const nextConfig = {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    domains: ['assets.coingecko.com'],
+    domains: ['assets.coingecko.com', 'coin-images.coingecko.com'],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
